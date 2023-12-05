a9f9f16f7cf64c4b443dfe31dd451a6427150e57
Sam H <shijwel@remotereach.com>
2023-12-05 16:58:59 +1100

    bug in kafka url

--- src/utils/kafka.sh
+++ src/utils/kafka.sh
@@ -4,7 +4,7 @@ if [ 'x'$1 == 'create' ];
 then
     mkdir kafka; 
     cd kafka;
-    git clone https://github.com/sulphurcrested/kafka;
+    git clone https://github.com/sulphurcrested/kafka.git;
 elif [ 'x'$1 == 'up' ];
 then
     docker-compose -f docker-compose.yml up --build --no-cache --detach kafka;
